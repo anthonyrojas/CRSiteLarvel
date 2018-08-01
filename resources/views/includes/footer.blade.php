5978d5b0cf4b32b50c06b9aef906802431120f11
Anthony Rojas <anthonyrojas@Anthonys-MBP.lan1>
2018-08-01 00:25:47 -0700

    footer style fixed

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,13 +1,19 @@
 <footer>
 	<div class="row">
-		<div class="col-md-4 footer-col">
-			Twitter
+		<div class="col-md-4 footer-col text-center">
+			<p class="text-center">Our Twitter</p>
+			<a href="https://twitter.com/CR_Normies" class="d-inline-block text-white footer-link">
+				<i class="fab fa-twitter fa-3x"></i>
+			</a>
 		</div>
 		<div class="col-md-4 footer-col">
-			This site was made with the intent of helping our clan members keep track of our data.
+			This site was made with the intent of helping our clan members keep track of our data. Created by Sir Doge. Pls give Doge coins.
 		</div>
 		<div class="col-md-4 footer-col">
-			Discord
+			<p class="text-center">Clash Royale</p>
+			<a href="https://https://clashroyale.com/" class="d-inline-block text-white footer-link">
+				<i class="fas fa-gamepad fa-3x"></i>
+			</a>
 		</div>
 	</div>
 </footer>
